f0f55da09188b974a78a900385785af89ea78cd9
John Teckemeyer <johnteckemeyer@gmail.com>
2021-12-07 10:37:19 -0800

    Create a linkedlist component that displays the list graphically

--- src/components/LinkedLists/LinkedLists.jsx
+++ src/components/LinkedLists/LinkedLists.jsx
@@ -1,7 +1,175 @@
 import React from 'react';
 
+import './LinkedLists.css';
+
+class Node {
+    constructor (data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    constructor () {
+        this.head = null;
+        this.size = 0;
+    }
+    
+    // Insert first node
+    insertFirst(data) {
+        this.head = new Node(data, this.head);
+        this.size++;
+    }
+
+    // Insert last node
+    insertLast(data) {
+        let node = new Node(data);
+        let current;
+
+        // If empty, make head
+        if (!this.head) {
+            this.head = node;
+        } else {
+            current = this.head;
+
+            while(current.next) {
+                current = current.next;
+            }
+
+            current.next = node;
+        }
+
+        this.size++
+    }
+
+    // Insert at index
+    insertAt(data, index) {
+        // If out of range
+        if (index < 0 || index > this.size) {
+            return;
+        }
+
+        // If first index
+        if (index === 0) {
+            this.head = new Node(data, this.head);
+            return;
+        }
+
+        const node = new Node(data);
+        let current, previous;
+
+        // Set current to first
+        current = this.head;
+        let count = 0;
+
+        while(count < index) {
+            previous = current;
+            count++;
+            current = current.next;
+        }
+
+        node.next = current;
+        previous.next = node;
+
+        this.size++;
+    }
+    
+    // Get at index
+    getAt(index) {
+        let current = this.head;
+        let count = 0;
+
+        while(current && count <= index) {
+            if (count === index) {
+                console.log(current.data);
+            }
+            count++;
+            current = current.next;
+        }
+
+        return null;
+    }
+
+    // Remove at index
+    removeAt(index) {
+        if (index < 0 || index > this.size) {
+            return;
+        }
+
+        let current = this.head;
+        let previous;
+        let count = 0;
+
+        // Remove first
+        if (index === 0) {
+            this.head = current.next;
+        } else {
+            while (count < index) {
+                count++;
+                previous = current;
+                current = current.next;
+            }
+
+            previous.next = current.next;
+        }
+
+        this.size--;
+    }
+
+    // Make Array
+    makeArray () {
+        const array = []
+        let current = this.head;
+        
+        while (current) {
+            array.push(current.data);
+            current = current.next;
+        }
+
+        return array;
+    }
+    
+    // Clear list
+    clearList () {
+        this.head = null;
+        this.size = 0;
+    }
+    
+    // Print list data
+    printListData () {
+        let current = this.head;
+
+        while (current) {
+            console.log(current.data);
+            current = current.next;
+        }
+    }
+}
+
+const ll = new LinkedList();
+
+ll.insertFirst(100);
+ll.insertFirst(200);
+ll.insertFirst(300);
+ll.insertLast(400);
+ll.insertAt(500, 2);
+ll.insertLast(600);
+ll.insertFirst(700);
+ll.insertAt(800, 1);
+ll.removeAt(4);
+
+ll.printListData();
+
+const llArray = ll.makeArray();
+
 export default function LinkedLists (props) {
     return (
-        <div>Linked Lists are cool!</div>
+        <div id="linkedlist">
+            {
+                llArray.map((node, index) => {
+                    return <div className="nodeDisplay" key={index}><span>{index}</span> {node}</div>
+                })
+            }
+        </div>
     )
 }
